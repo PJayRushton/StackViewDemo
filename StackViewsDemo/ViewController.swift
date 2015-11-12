2e18bd4e068026562f572b6bc788636572f0f26c
PJayRushton <parker.rushton@octanner.com>
2015-11-12 15:17:28 -0700

    Initial Commit

---
+++ StackViewsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewsDemo
+//
+//  Created by Parker Rushton on 11/12/15.
+//  Copyright © 2015 Parker Rushton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
