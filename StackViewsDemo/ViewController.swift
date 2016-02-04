08c9a34ffdf7a9d6f867357a054d77f269021589
PJayRushton <parker.rushton@octanner.com>
2016-02-04 10:35:23 -0700

    Adds animation code

--- StackViewsDemo/ViewController.swift
+++ StackViewsDemo/ViewController.swift
@@ -9,35 +9,73 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var stack: UIStackView!
     
+    @IBOutlet weak var axisButton: UIButton!
+    @IBOutlet weak var mainStack: UIStackView!
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var animatedSwitch: UISwitch!
+    @IBOutlet weak var orangeButton: UIButton!
+    @IBOutlet weak var orangeView: UIView!
+    @IBOutlet weak var alphaSwitch: UISwitch!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         
-        stack.axis = UILayoutConstraintAxis.Horizontal
-        stack.leadingAnchor
+        navigationController?.navigationBarHidden = true
     }
     
-    func changeAxis(animated animated: Bool) {
-        if animated {
-            UIView.animateWithDuration(1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 1, options: UIViewAnimationOptions.LayoutSubviews, animations: { () -> Void in
-                self.stack.axis = self.stack.axis == .Vertical ? .Horizontal : .Vertical
-                self.button.setTitle(self.stack.axis == .Vertical ? "Vertical" : "Horizontal", forState: .Normal)
-                self.stack.subviews.last!.hidden = !self.stack.subviews.last!.hidden
-                }, completion: nil)
+    
+    // MARK: - IBActions
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        toggleAxis()
+    }
+    
+    @IBAction func removeOrangeButtonPressed(sender: UIButton) {
+        toggleOrangeView(!orangeView.hidden)
+    }
+    
+    
+    // MARK: - Private functions
+    
+    private func toggleAxis() {
+        if animatedSwitch.on {
+            UIView.animateWithDuration(1, animations: { () -> Void in
+                self.mainStack.axis = self.mainStack.axis == .Vertical ? .Horizontal : .Vertical
+                }, completion: { success in
+                    self.handleAxisChangeComplete()
+//                    self.removeLastSubview() // Uncomment to see what removing the last arrangedSubview looks like
+            })
         } else {
-            stack.axis = stack.axis == .Vertical ? .Horizontal : .Vertical
-            self.button.setTitle(self.stack.axis == .Vertical ? "Vertical" : "Horizontal", forState: .Normal)
-            stack.setNeedsLayout()
+            mainStack.axis = mainStack.axis == .Vertical ? .Horizontal : .Vertical
+            handleAxisChangeComplete()
+//            removeLastSubview() // Uncomment to see what removing the last arrangedSubview looks like
         }
     }
-
-    @IBAction func buttonPressed(sender: AnyObject) {
-        changeAxis(animated: true)
+    
+    private func handleAxisChangeComplete() {
+        axisButton.setTitle(mainStack.axis == .Horizontal ? "Horizontal" : "Veritcal", forState: .Normal)
     }
-
+    
+    private func removeLastSubview(hidden: Bool = true) { // If `false` views are removed from `arrangedSubviews` but still visible ( and no longer managed by the stackView)
+            let last = mainStack.arrangedSubviews.last!
+            last.hidden = hidden
+            mainStack.removeArrangedSubview(last)
+            print("Subviews: \(mainStack.subviews.count)")
+            print("ArrangedSubviews: \(mainStack.arrangedSubviews.count)")
+    }
+    
+    private func toggleOrangeView(remove: Bool) {
+        if animatedSwitch.on {
+            UIView.animateWithDuration(0.5, animations: {
+                self.orangeView.hidden = remove
+                self.orangeView.alpha = self.alphaSwitch.on && remove ? 0 : 1
+            })
+        } else {
+            orangeView.hidden = remove
+        }
+        orangeButton.setTitle(orangeView.hidden ? "Add" : "Remove", forState: .Normal)
+    }
+    
 }
-
