fe3063ae536b3be4c907970b046119fb9f25ba36
PJayRushton <parker.rushton@octanner.com>
2016-02-04 10:35:35 -0700

    Adds SecondViewController

---
+++ StackViewsDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  StackViewsDemo
+//
+//  Created by Parker Rushton on 2/4/16.
+//  Copyright © 2016 Parker Rushton. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+
+}
